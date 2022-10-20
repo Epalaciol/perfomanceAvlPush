e6a2a3cdb828cabe545b4f757b720745f78d6094
Esteban Palacio <esteban.1054@hotmail.com>
2022-10-20 16:19:02 -0500

    set up

---
+++ index.js
@@ -0,0 +1,8 @@
+import dgram from 'dgram';
+import { Buffer } from 'buffer';
+
+const message = Buffer.from('Some bytes');
+const client = dgram.createSocket('udp4');
+client.send(message, 41234, 'localhost', (err) => {
+  client.close();
+});
